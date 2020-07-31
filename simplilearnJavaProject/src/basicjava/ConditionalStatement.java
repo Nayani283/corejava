0da46e255df89f828f91c4be7abd66aa4de5efcc
Nayani283 <nayanipriya93@gmail.com>
2020-07-31 23:27:27 +0530

    basic java

---
+++ simplilearnJavaProject/src/basicjava/ConditionalStatement.java
@@ -0,0 +1,22 @@
+package basicjava;
+
+import java.util.Scanner;
+
+public class ConditionalStatement {
+
+	public static void main(String[] args) {
+		Scanner s1=new Scanner(System.in);
+		System.out.println("enter y/n:");
+		char resp = s1.nextLine().charAt(0);
+		if(resp == 'y'){
+			System.out.println("you entered yes");
+			
+		}
+		else {
+			System.out.println("you enetred no");
+		}
+		
+     s1.close();
+	}
+
+}
